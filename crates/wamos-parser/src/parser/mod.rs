5ce94f33eb4da975b8ec6f3f9e07d10ed5292951
Ludwig Stecher <ludwig.stecher@gmx.de>
2021-01-31 01:43:16 +0100

    Add nice test

--- crates/wamos-parser/src/parser/mod.rs
+++ crates/wamos-parser/src/parser/mod.rs
@@ -123,3 +123,215 @@ trait Parse: Sized {
         }
     }
 }
+
+
+#[cfg(test)]
+mod tests {
+    fn parsed_equals(code: &str, out: &str) {
+        let lexed = crate::lexer::lex(code);
+        assert_eq!(lexed.errors(), vec![]);
+        match super::parse(lexed.tokens()) {
+            Ok(items) => {
+                let actual = format!("{:#?}", items);
+                let changes = difference::Changeset::new(&actual, out, "\n");
+                if changes.distance > 0 {
+                    eprintln!("{}", changes);
+                    panic!(
+                        "{} differences between expected and actual output",
+                        changes.distance
+                    );
+                }
+            }
+            Err(err) => {
+                panic!("{}", err);
+            }
+        }
+    }
+
+    #[test]
+    fn test() {
+        parsed_equals(
+            "fun foo[T](x List[T], y) List[T] {
+    x = x.map(y) List[T];
+    assert(x.len != 0);
+    x
+}",
+            "[
+    Function(
+        Function {
+            name: Ident(
+                Ident #1,
+            ) @ 4..7,
+            generics: [
+                GenericParam {
+                    name: UpperIdent #2 @ 8..9,
+                    bounds: [],
+                } @ 8..9,
+            ] @ 7..10,
+            args: [
+                FunArgument {
+                    name: Ident #3,
+                    ty: Some(
+                        NamedType {
+                            name: UpperIdent #4 @ 13..17,
+                            args: [
+                                Type(
+                                    NamedType {
+                                        name: UpperIdent #2 @ 18..19,
+                                        args: [] @ 0..0,
+                                    },
+                                ) @ 18..19,
+                            ] @ 17..20,
+                        } @ 13..20,
+                    ),
+                    default: None,
+                } @ 11..20,
+                FunArgument {
+                    name: Ident #5,
+                    ty: None,
+                    default: None,
+                } @ 22..23,
+            ] @ 10..24,
+            return_ty: Some(
+                NamedType {
+                    name: UpperIdent #4 @ 25..29,
+                    args: [
+                        Type(
+                            NamedType {
+                                name: UpperIdent #2 @ 30..31,
+                                args: [] @ 0..0,
+                            },
+                        ) @ 30..31,
+                    ] @ 29..32,
+                } @ 25..32,
+            ),
+            body: Some(
+                Block {
+                    exprs: [
+                        Assignment(
+                            Assignment {
+                                lhs: Invokable(
+                                    Invokable {
+                                        name: Ident(
+                                            Ident #3,
+                                        ) @ 39..40,
+                                        generics: [] @ 0..0,
+                                    },
+                                ) @ 39..40,
+                                rhs: TypeAscription(
+                                    TypeAscription {
+                                        expr: ParenCall(
+                                            ParenCall {
+                                                receiver: MemberCall(
+                                                    MemberCall {
+                                                        receiver: Invokable(
+                                                            Invokable {
+                                                                name: Ident(
+                                                                    Ident #3,
+                                                                ) @ 43..44,
+                                                                generics: [] @ 0..0,
+                                                            },
+                                                        ) @ 43..44,
+                                                        member: Invokable {
+                                                            name: Ident(
+                                                                Ident #6,
+                                                            ) @ 45..48,
+                                                            generics: [] @ 0..0,
+                                                        },
+                                                    },
+                                                ) @ 43..48,
+                                                args: Some(
+                                                    [
+                                                        FunCallArgument {
+                                                            name: None,
+                                                            expr: Invokable(
+                                                                Invokable {
+                                                                    name: Ident(
+                                                                        Ident #5,
+                                                                    ) @ 49..50,
+                                                                    generics: [] @ 0..0,
+                                                                },
+                                                            ) @ 49..50,
+                                                        } @ 49..50,
+                                                    ],
+                                                ),
+                                            },
+                                        ) @ 43..51,
+                                        ty: NamedType {
+                                            name: UpperIdent #4 @ 52..56,
+                                            args: [
+                                                Type(
+                                                    NamedType {
+                                                        name: UpperIdent #2 @ 57..58,
+                                                        args: [] @ 0..0,
+                                                    },
+                                                ) @ 57..58,
+                                            ] @ 56..59,
+                                        },
+                                    },
+                                ) @ 43..59,
+                            },
+                        ) @ 39..59,
+                        ParenCall(
+                            ParenCall {
+                                receiver: Invokable(
+                                    Invokable {
+                                        name: Ident(
+                                            Ident #7,
+                                        ) @ 65..71,
+                                        generics: [] @ 0..0,
+                                    },
+                                ) @ 65..71,
+                                args: Some(
+                                    [
+                                        FunCallArgument {
+                                            name: None,
+                                            expr: Operation(
+                                                Operation {
+                                                    operator: Operator #9,
+                                                    lhs: MemberCall(
+                                                        MemberCall {
+                                                            receiver: Invokable(
+                                                                Invokable {
+                                                                    name: Ident(
+                                                                        Ident #3,
+                                                                    ) @ 72..73,
+                                                                    generics: [] @ 0..0,
+                                                                },
+                                                            ) @ 72..73,
+                                                            member: Invokable {
+                                                                name: Ident(
+                                                                    Ident #8,
+                                                                ) @ 74..77,
+                                                                generics: [] @ 0..0,
+                                                            },
+                                                        },
+                                                    ) @ 72..77,
+                                                    rhs: Literal(
+                                                        Int(0),
+                                                    ) @ 81..82,
+                                                },
+                                            ) @ 72..82,
+                                        } @ 72..82,
+                                    ],
+                                ),
+                            },
+                        ) @ 65..83,
+                        Invokable(
+                            Invokable {
+                                name: Ident(
+                                    Ident #3,
+                                ) @ 89..90,
+                                generics: [] @ 0..0,
+                            },
+                        ) @ 89..90,
+                    ],
+                    ends_with_semicolon: false,
+                } @ 33..92,
+            ),
+        },
+    ) @ 0..92,
+]",
+        );
+    }
+}
